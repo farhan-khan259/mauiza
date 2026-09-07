97f34657e0b3357e0db5974ae0b463b6a1f060e2
Farhan Khan <farhan-khan259@users.noreply.github.com>
2026-09-07 22:12:31 +0500

    style: format ScrollToTop component for improved readability

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -1,1 +1,10 @@
-import {useEffect} from 'react';import {useLocation} from 'react-router-dom';import './ScrollToTop.css'; export default function ScrollToTop(){const {pathname}=useLocation();useEffect(()=>window.scrollTo({top:0,behavior:'auto'}),[pathname]);return null}
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import "./ScrollToTop.css";
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+  }, [pathname]);
+  return null;
+}
